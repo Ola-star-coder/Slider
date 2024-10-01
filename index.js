f7d866e6d579316b0d64f4faa3eeb314a2a3b30c
olamide <olamideashaolu318.gmail.com>
2024-10-01 21:40:03 +0100

    commit new codes

--- index.js
+++ index.js
@@ -20,3 +20,35 @@ var swiper = new Swiper(".mySwiper", {
                 prevEl: ".swiper-button-prev",
             },
         });
+
+
+    new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
